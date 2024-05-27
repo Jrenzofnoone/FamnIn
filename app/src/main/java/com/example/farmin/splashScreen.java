8557c0c0e58144473da0b5149e87630c64a32777
HellsLake <127571027+Jrenzofnoone@users.noreply.github.com>
2024-05-27 18:29:38 +0800

    splash screen

---
+++ app/src/main/java/com/example/farmin/splashScreen.java
@@ -0,0 +1,32 @@
+package com.example.farmin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashScreen extends AppCompatActivity {
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(splashScreen.this, landingPage.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
